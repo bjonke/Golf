390a5ba40f18fee74691b64b6db6c14c02b96897
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-09-16 16:59:39 +0200

    Update Players.h
    Added a Player class to replace the old struct

--- Players.h
+++ Players.h
@@ -3,7 +3,7 @@
 * @brief this header file will contain all required 
 * definitions and basic utilities functions.
 *
-* @author RISK
+* @author bjonke
 *
 * @date 2012-07-27
 */
@@ -11,7 +11,33 @@
 #ifndef PLAYERS
 #define PLAYERS
 
+#include <string>
 #include "Functions.h"
+
+class Player
+{
+	public:
+	Player();
+	~Player();
+	void PickPlayers();
+	
+	private:
+	// Tournament member
+	bool tournament;
+	// Currently the active player
+	bool active;
+	// Name of player
+	std::string name;
+	// old variables
+	int players;
+	int playeractived;
+	int finished_hole;
+	bool picked;
+	bool finished;
+	char names[4][30];
+	int nameplace[4];
+	int selectMap;
+};
 
 struct Players
 {
