e0de8da3394dfa9cdbbb3eaf861a75a9eb874dc6
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-09-20 17:08:07 +0200

    Update skjut.cpp
    Cleaning up and rewriting function code.

--- skjut.cpp
+++ skjut.cpp
@@ -15,11 +15,139 @@ extern int **yta;
 //extern float hojd1,bredd1;
 using namespace std;
 
+// Replacement function for Skjut and should be added to the header file later
+bool Shoot(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,float &atY,bool &ground)
+{
+	boll.current_height += boll.height;
+	if(boll.current_height > 0)
+	{
+		if(yta[(int)boll.x / 50][(int)boll.y / 50] == 4)
+		{
+			int CenterX = (int)(boll.x / 50 )*50+25;
+			int CenterY = (int)(boll.y / 50 )*50+17;
+			static int CX=-1,CY=-1;
+			
+			// What the BLIP! is this?
+			bool tempbool=false;
 
+			float tempX = boll.x-atX,tempY = boll.y - atY;
+			float malLangd = sqrtf(powf(CenterX - tempX,2.0) + powf(CenterY - tempY,2.0));
+			float bollLangd;
+			
+			if(boll.distance>malLangd)
+			{
+				bollLangd=malLangd;
+			}
+			else
+			{
+				bollLangd=boll.distance;
+			}
 
-bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,float &atY,bool &ground) //Allt som har med bollens skjutande att göra
-{	
+			tempX+=atX*bollLangd;
+			tempY+=atY*bollLangd;
+
+			if(sqrtf(powf((tempX - CenterX),2.0) + powf((tempY - CenterY),2.0)) < 15 && (CX!=CenterX || 
+						CY!=CenterY) && boll.current_height < 300) //Kollar om den rÃ¶r trÃ¤d
+			{
+				atX = (atX*boll.distance + Windpos.x) / (boll.distance);
+				atY = (atY*boll.distance + Windpos.y) / (boll.distance);
+				Windpos.x = 0;
+				Windpos.y = 0;
+				if(boll.height > 0)
+				{
+					boll.height = 0;
+				}
+				boll.distance /= 2;
+
+				CX = CenterX;
+				CY = CenterY;
+				if(abs(CenterX - tempX) < 4)
+				{
+					atY = -atY;
+				}
+				else if(abs(CenterY-tempY) < 4)
+				{
+					atX= -atX;
+				}
+				else if( (tempX > CenterX && tempY > CenterY) || (tempX < CenterX && tempY < CenterY))
+				{
+					float temp=atX;
+					atX= -atY;
+					atY= -temp;
+				}
+				else
+				{
+					//swap(atX,atY);
+					float temp=atX;
+					atX= atY;
+					atY= temp;
+				}
+				snd.Three(); //Spelar trÃ¤ff av trÃ¤d
+			}		
+		}
+
+		boll.height-=0.1f;	
+		return true;
+	}
+
+	boll.current_height=0;
+
+	if(ground)	//ifall den nÃ¥tt marken sÃ¥ sÃ¤tts vinkeln
+	{
+		boll.distance*=0.7;
+		//if(Windpos.x!=0 )
+		atX= (atX*boll.distance + Windpos.x)/(boll.distance);
+		//if(Windpos.y!=0) 
+		atY= (atY*boll.distance + Windpos.y)/(boll.distance);
+
+		Windpos.x=0;
+		Windpos.y=0;
+		ground=false;
+	}	
+	
+	int var= yta[(int)boll.x/50][(int)boll.y/50]%50; //var bollen Ã¤r
+
+	switch(var)
+	{
+		case 13:
+			InGoal(play);
+			break;
+		case 1:
+			boll.distance-=0.1;
+			break;
+		case 2:
+		case 5:
+		case 6:
+		case 7: 
+		case 8: boll.distance=0;
+			snd.Water();
+			break;
+		case 3:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+			boll.distance-=0.5;
+			break;
+		case 4:
+			InTerrain(play);
+			boll.distance=0;
+			break;
+		}
 		
+		if(boll.distance>0)
+		{
+			return true;
+		}
+
+	ground=true;
+	InTerrain(play);
+	
+	return false;	
+}
+
+bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,float &atY,bool &ground) //Allt som har med bollens skjutande att gÃ¶ra
+{	
 	boll.current_height+=boll.height;
 	if(boll.current_height>0)
 	{
@@ -48,7 +176,7 @@ bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,floa
 			
 			
 
-			if(sqrtf(powf((tempX-CenterX),2.0) + powf((tempY-CenterY),2.0)) <15 && (CX!=CenterX || CY!=CenterY) && boll.current_height < 300) //Kollar om den rör träd
+			if(sqrtf(powf((tempX-CenterX),2.0) + powf((tempY-CenterY),2.0)) <15 && (CX!=CenterX || CY!=CenterY) && boll.current_height < 300) //Kollar om den rÃ¶r trÃ¤d
 			{
 				
 				atX= (atX*boll.distance + Windpos.x)/(boll.distance);
@@ -79,7 +207,7 @@ bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,floa
 					atX= atY;
 					atY= temp;
 				}
-				snd.Three(); //Spelar träff av träd
+				snd.Three(); //Spelar trÃ¤ff av trÃ¤d
 			}		
 		}
 
@@ -89,7 +217,7 @@ bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,floa
 
 	boll.current_height=0;
 
-	if(ground)	//ifall den nått marken så sätts vinkeln
+	if(ground)	//ifall den nÃ¥tt marken sÃ¥ sÃ¤tts vinkeln
 	{
 	
 	
@@ -103,7 +231,7 @@ bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,floa
 		Windpos.y=0;
 		ground=false;
 	}	
-		int var= yta[(int)boll.x/50][(int)boll.y/50]%50; //var bollen är
+		int var= yta[(int)boll.x/50][(int)boll.y/50]%50; //var bollen Ã¤r
 
 		switch(var)
 		{
@@ -143,14 +271,14 @@ bool Skjut(IsPlaying &play,golf_ball_position &boll,Pos &Windpos,float &atX,floa
 	return false;
 }
 
-void hitBoll(GolfClub klubba,golf_ball_position &boll) //initierar bollens möjligheter
+void hitBoll(GolfClub klubba,golf_ball_position &boll) //initierar bollens mÃ¶jligheter
 {
 	boll.height = klubba.height;
 	boll.distance = klubba.distance;		
 }
 
 
-Direction CompPosX(float bredd1,float position) // kollar om vart den ska röra sig med hjälp av musen
+Direction CompPosX(float bredd1,float position) // kollar om vart den ska rÃ¶ra sig med hjÃ¤lp av musen
 {
 	if(position-bredd1 > 600) 
 		return Right;
@@ -161,7 +289,7 @@ Direction CompPosX(float bredd1,float position) // kollar om vart den ska röra s
 	return None;
 }
 
-Direction CompPosY(float hojd1,float position) // kollar om vart den ska röra sig med hjälp av musen
+Direction CompPosY(float hojd1,float position) // kollar om vart den ska rÃ¶ra sig med hjÃ¤lp av musen
 {
 	if(position-hojd1 > 450) 
 		return Down;
@@ -173,7 +301,7 @@ Direction CompPosY(float hojd1,float position) // kollar om vart den ska röra si
 }
 
 
-bool CheckXY(float x,float y) //Kollaro m dne är innanför skärmen
+bool CheckXY(float x,float y) //Kollaro m dne Ã¤r innanfÃ¶r skÃ¤rmen
 {
 	if(x>0 && x<800 && y>0 && y<600)
 		return true;
@@ -183,14 +311,14 @@ bool CheckXY(float x,float y) //Kollaro m dne är innanför skärmen
 bool OutofBounds(golf_ball_position &boll,float atX,float atY)
 {
 	bool out=false;
-	while(boll.x <= 0 || boll.x >= bredd*50-1 || boll.y<=0 || boll.y >= hojd*50-1) // sätter den till kanten
+	while(boll.x <= 0 || boll.x >= bredd*50-1 || boll.y<=0 || boll.y >= hojd*50-1) // sÃ¤tter den till kanten
 	{
 		boll.x -=atX;
 		boll.y -=atY;
 		out=true;
 	}
 		
-	if(out==true)	//gör så att den får 2 extra värden
+	if(out==true)	//gÃ¶r sÃ¥ att den fÃ¥r 2 extra vÃ¤rden
 	{
 		int bx=(int)boll.x/50 , by=(int)boll.y/50;		
 
@@ -262,8 +390,6 @@ void InGoal(IsPlaying & play)
 
 	
 }
-
-
 
 template<class T> 
 T Biggest(T ett, T tva)
