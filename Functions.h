7473b2fc21cd278b6330955a4cb1edadb58774f1
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-04 15:55:40 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -15,10 +15,16 @@
 
 //struct Pos;
 //struct Bollpos;
+// Replacement for Windrandom function
+void WindEffect(float &x,float &y,float &max);
+// Replacement function for drawcircle function
+void DrawGolfBall(int x, int y, int r, int c);
+
 void Windrandom(float &x,float &y,float &max);
 void UseWind(bool &startMove, Pos &boll,Pos &WPos);
 void DrawIMGAlpha(SDL_Surface *img,int x, int y, int w, int h, int x2, int y2, int trans, bool alpha_on);
 void DrawLine(float startX,float startY,float stopX,float stopY);
+
 void drawcircle(int x, int y, int r, int c);
 void Matare(golf_ball_position &boll);
 float Power(bool &first);
