2378e4ed94bc5e3ccf0ab6c02797d7e205e87acb
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-04 16:17:46 +0200

    Update item.h

--- item.h
+++ item.h
@@ -1,9 +1,8 @@
 /** 
 * @file item.h 
-* @brief this header file will contain all required 
-* definitions and basic utilities functions.
+* @brief 
 *
-* @author RISK
+* @author demo
 *
 * @date 2012-07-27
 */
@@ -13,26 +12,12 @@
 #include <iostream>
 using namespace std;
 
-	/**
-	* This method will be used for sound.
-	* @author RISK
-	* @date 2012-07-27
-	*/
 class item
 {
 public:
 private:
-		/**
-	* This method will be used for sound.
-	* @author RISK
-	* @date 2012-07-27
-	*/
+
 	item();
-		/**
-	* This method will be used for sound.
-	* @author RISK
-	* @date 2012-07-27
-	*/
 	~item();
 }
 #endif
