b340c3f73b289f9f2994e3458284070b62113146
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-21 16:37:56 +0200

    Update FMain.h

--- FMain.h
+++ FMain.h
@@ -4,49 +4,15 @@
 #include "playerinput.h"
 #include "Players.h"
 
-/**
-* This method will be used to pick a player.
-* @author RISK
-* @date 2012-07-27
-*/
 void PickPlayers();
 
-/**
-* This method will be used for LandSatt.
-* @author RISK
-* @param &isp something something
-* @date 2012-07-27
-*/
 void LandSatt(int &isp);
 void CheckBallLanded(int &current_player);
-
-/**
-* This method will be used for main events.
-* @author RISK
-* @date 2012-07-27
-*/
 void MainEvents(Bools &bo,float &Rhojd,float &RBredd,int x2,int y2,GolfClub &klubb,GolfClub klubbor[],int &isp,float &value);
 
-/**
-* This method will be used for Shooting.
-* @author RISK
-* @date 2012-07-27
-*/
 void MainSkjut(bool &set,int &x2,int &y2,int &isp,GolfClub &klubb,Bools &bo, float &value,Pos &windPos,Pos &WPos);
-
-/**
-* This method will be used for Shooting.
-* @author RISK
-* @brief This method will be used for Shooting.
-* @date 2012-07-27
-*/
 void FireGolfBall(bool &set,int &x2,int &y2,int &isp,GolfClub &golfclub,Bools &bo, float &value,Pos &windPos,Pos &WPos);
 
-/**
-* This method will be used for handling events.
-* @author RISK
-* @date 2012-07-27
-*/
 void EventHandler(Bools &bo,float &RHeight,float &RWidth,int x2,int y2,GolfClub &golfclub,GolfClub golfclubs[],int &isp,float &value);
 
 #endif
