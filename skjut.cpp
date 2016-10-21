9ece77e5806c6b022eb826a4a0fe712bc2366d3f
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-21 16:13:43 +0200

    Update skjut.cpp

--- skjut.cpp
+++ skjut.cpp
@@ -1,13 +1,3 @@
-/** 
-* @file skjut.cpp 
-* @brief this header file will contain all required 
-* definitions and basic utilities functions.
-*
-* @author RISK
-*
-* @date 2012-07-27
-*/
-
 #include "skjut.h"
 //#include "funktioner.h"
 //#include <math.h>
