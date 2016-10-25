ed8fa3c34170fca8e1e4ef8c41fdf71a1dd2e798
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-26 00:10:48 +0200

    Update golf_club.h

--- golf_club.h
+++ golf_club.h
@@ -6,26 +6,11 @@ class GolfClub
 {
 public:
 	
-	// What are these values for?
-	float height;
-	float length;
-	
 	GolfClub(){};
-	GolfClub(float height,float length){ this->height = height, this->length=length; };	
+	GolfClub(float IHeight,float iLength){ mHeight = IHeight, mLength=iLength; };	
 	~GolfClub(){};
+private:
+	float mHeight;
+	float mLength;
+
 };
-
-/**
-* This method will be used for sound.
-* @author RISK
-* @date 2012-07-27
-*/
-struct golf_club
-{
-	float height;
-	float length;
-
-	golf_club(){}
-	golf_club(float height,float length){this->height = height, this->length=length;}	
-};
-
