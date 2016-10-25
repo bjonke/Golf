f996d595b00e0b50dd477cf362d23d345bcc84d7
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-26 00:19:39 +0200

    Update skjut.h

--- skjut.h
+++ skjut.h
@@ -11,25 +11,6 @@ T Smallest(T ett, T tva);
 
 enum Direction{Up,Down,Right,Left,None};
 
-struct Pos
-{
-	float x;
-	float y;
-};
-
-/*
-struct golf_ball_position
-{
-	Pos Position;
-	//float x;
-	//float y;
-	float height;
-	float distance;
-	float current_height;
-	golf_ball_position() : current_height(0) {}
-};
-*/
-
 class GolfBall
 {
 public:
@@ -37,19 +18,27 @@ public:
 	float height;
 	float distance;
 	float currentHeight;
-	GolfBall() : currentHeight(0.0f) , x(0.0f) , y(0.0f) , distance(0.0f), height(0.0f) {};
+	bool Inhole;
+	bool Landed;
+	
+	GolfBall() : currentHeight(0.0f) , x(0.0f) , y(0.0f) , distance(0.0f), height(0.0f), InHole = (false) {};
 	
 	float getHeight() { return height; };
 	float getX() { return x; };
 	float getY() { return y; };
 	float getDistance() { return distance; };
 	float getCurrentHeight() { return currentHeight; };
+	bool getInHole() { return InHole; };
+	bool getLanded() { return Landed; };
 	
 	float setHeight(float iHeight ) { height = iHeight; };
 	float setX( float iX ) { x = iX; };
 	float setY( float iY ) { y= iY; };
 	float setDistance( float iDistance ) { distance = iDistance; };
 	float setCurrentHeight( float iCurrentHeight ) { currentHeight = iCurrentHeight; };
+	bool setInHole( bool YesNo ) { InHole = YesNo; };
+	bool setLanded( bool YesNo ) { Landed = YesNo; };
+	
 };
 
 struct IsPlaying
@@ -60,9 +49,10 @@ struct IsPlaying
 	//bool landat;
 	int score;
 	//IsPlaying(){sattbollen=false; landat=false; score=0;}
-	golf_ball_position ball;
-	bool BallInHole;
-	bool BallLanded;
+	//golf_ball_position ball;
+	GolfBall Ball;
+	//bool BallInHole;
+	//bool BallLanded;
 	IsPlaying(){BallInHole=false; BallLanded=false; score=0;}
 };
 
@@ -74,6 +64,7 @@ bool Skjut(IsPlaying &play, GolfBall &Ball ,Pos &Windpos,float &atX,float &atY,b
 
 Direction CompPosX(float bredd,float positiong);
 Direction CompPosY(float hojd,float positiong);
+
 bool CheckXY(float X,float Y);
 // Old function with golf_ball_position replaced
 //bool OutofBounds(golf_ball_position &boll,float tempX,float tempY);
