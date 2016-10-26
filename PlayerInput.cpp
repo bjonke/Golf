39817ca492efde49ad05327b30a56823e7f1634d
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2016-10-26 22:54:01 +0200

    Update PlayerInput.cpp

--- PlayerInput.cpp
+++ PlayerInput.cpp
@@ -6,8 +6,9 @@
 //extern SDL_Surface* screen;
 //extern float bredd1,hojd1;
 
-#define PITCH (surf.ScreenSurface->pitch/4)
+#define PITCH (DSurface.ScreenSurface->pitch/4)
 
+// Drawing text character on screen, this should be replaced with SDL Font code
 void drawcharacter(int x, int y, int character)
 {	
   //if (character == 32)
@@ -16,51 +17,49 @@ void drawcharacter(int x, int y, int character)
   character -= 33; // our font does not have the first 33 characters.
 
   // Lock surface if needed
-  if (SDL_MUSTLOCK(surf.FontSurface))
-	  if (SDL_LockSurface(surf.FontSurface) < 0) 
+  if (SDL_MUSTLOCK(DSurface.FontSurface))
+	  if (SDL_LockSurface(DSurface.FontSurface) < 0) 
       return;
 
   int i, j;
-  for (i = 0; i < surf.FontSurface->w; i++)
+  for (i = 0; i < DSurface.FontSurface->w; i++)
   {
     int screenofs = x + (y + i) * PITCH;
-	int charofs = (i + character * surf.FontSurface->w) * (surf.FontSurface->pitch / 4);
-	for (j = 0; j < surf.FontSurface->w; j++)
+	int charofs = (i + character * DSurface.FontSurface->w) * (DSurface.FontSurface->pitch / 4);
+	for (j = 0; j < DSurface.FontSurface->w; j++)
     {
-		((unsigned int*)surf.ScreenSurface->pixels)[screenofs] = 
-			((unsigned int*)surf.FontSurface->pixels)[charofs];
+		((unsigned int*)DSurface.ScreenSurface->pixels)[screenofs] = 
+			((unsigned int*)DSurface.FontSurface->pixels)[charofs];
       screenofs++;
       charofs++;
     }
   }
 
   // Unlock if needed
-  if (SDL_MUSTLOCK(surf.FontSurface)) 
-		SDL_UnlockSurface(surf.FontSurface);
+  if (SDL_MUSTLOCK(DSurface.FontSurface)) 
+		SDL_UnlockSurface(DSurface.FontSurface);
 }
 
-
+// Also related to BMP text drawing and should be replaced
 void drawstring(int x, int y, char *s)
 {		
   while (*s != 0)
   {
     drawcharacter(x, y, *s);
     s++;
-	x += surf.FontSurface->w;
+	x += DSurface.FontSurface->w;
   }
 }
 
 bool Events(SDL_Surface *img,Players &pl)
 {
 	int x,y;
+	bool start=false;
+	int tx=419,ty=448,plus=37;
+	SDL_Event event;
 
 	SDL_GetMouseState(&x,&y);
-	
-	bool start=false;
 
-	int tx=419,ty=448,plus=37;
-
-	SDL_Event event;
 	while(SDL_PollEvent(&event))
 		{
 			switch(event.type)
@@ -69,10 +68,13 @@ bool Events(SDL_Surface *img,Players &pl)
 				if(event.button.button == SDL_BUTTON_LEFT)
 				{					
 					if(x>102 && x< 328 && y>25 && y<241)
+					{
 						pl.tournament=false;
+					}
 					else if(x>398 && x< 626 && y>25 && y<241)
+					{
 						pl.tournament=true;
-
+					}
 					else if(x>438 && y>492 && x<568 && y< 552)
 					{
 						for(int i=0; i< pl.players ; i++)
@@ -86,28 +88,31 @@ bool Events(SDL_Surface *img,Players &pl)
 							{
 								start=true;
 							}
-						}
-
-						
+						}	
 					}
-
 					else if(x>2 && x<68 && y>49 && y<115)
 					{
 						int temp=(68-2)/3;						
 						pl.selectMap=(x-2)/temp+1+3*((y-49)/temp);
 					}
-
 					else if(x>54 && x<87)
 					{	
-
 						if(y>tx && y<ty)
+						{
 							pl.players=1;
+						}
 						else if(y>tx+plus && y<ty+plus)
+						{
 							pl.players=2;
+						}
 						else if(y>tx+plus*2 && y<ty+plus*2)
+						{
 							pl.players=3;
+						}
 						else if(y>tx+plus*3 && y<ty+plus*3)
+						{
 							pl.players=4;
+						}
 
 						pl.playeractivated=0;
 					}
@@ -116,16 +121,26 @@ bool Events(SDL_Surface *img,Players &pl)
 						pl.playeractivated=0;
 
 						if(y>tx && y<ty)
+						{
 							pl.playeractivated=1;
+						}
 						else if(y>tx+plus && y<ty+plus && pl.players>1)
+						{
 							pl.playeractivated=2;
+						}
 						else if(y>tx+plus*2 && y<ty+plus*2 && pl.players>2)
+						{
 							pl.playeractivated=3;
+						}
 						else if(y>tx+plus*3 && y<ty+plus*3 && pl.players>3)
+						{
 							pl.playeractivated=4;
+						}
 					}					
 					else
-						pl.playeractivated=0;					
+					{
+						pl.playeractivated=0;
+					}
 				}
 				break;
 
@@ -149,7 +164,7 @@ bool Events(SDL_Surface *img,Players &pl)
 			case SDL_KEYDOWN:	
 				switch(event.key.keysym.sym)
 				{
-				
+				// Remove this bool crap
 				case SDLK_ESCAPE:
 					extern Bools bo;
 					bo.Done=true;
@@ -159,7 +174,6 @@ bool Events(SDL_Surface *img,Players &pl)
 				break;
 			}
 			break;
-
 	}
 	drawstring(104,426,pl.names[0]);
 	drawstring(104,426+plus,pl.names[1]);
@@ -167,65 +181,56 @@ bool Events(SDL_Surface *img,Players &pl)
 	drawstring(104,426+plus*3,pl.names[3]);
 
 	for(int i=4; i>pl.players; i--)
+	{
 		DrawIMGAlpha(img,100,419+plus*(i-1),261,29,100,600,150,true);
+	}
 	
 	if(pl.tournament)
 	{		
 		for(int i=0; i<7; i++)
+		{
 			DrawIMGAlpha(img,103,26+31*i,228,31,367,600,60,true);
+		}
 	}
 	else
 	{
 		for(int i=0; i<7; i++)
+		{
 			DrawIMGAlpha(img,398,26+31*i,228,31,367,600,60,true);
+		}
 	}
 	for(int i=0; i<3; i++)
 		for(int j=0; j<3; j++)
 		{
 			if(j+1+i*3 != pl.selectMap)
+			{
 				DrawIMGAlpha(img,4+22*j,51+22*i,19,19,367,600,60,true);
+			}
 		}
-		
-
 	return start;
-
-
-	
 }
-/*	
-Players::Players()
-{
-	selectMap=1;
-	players=1;
-	playeractivated=0;
-	tournament=false;
-	antalsomsattden=0;
-	picked=false;
-	finished=false;
 
-	for(int i=0; i<4; i++)
-	{
-		names[i][0]='\0';
-		nameplace[i]=0;
-	}
-}
-*/
-
-
-//bollpos parameter
 void BreddHojd(golf_ball_position boll)
 {
 	bredd1=(float)boll.x-400.0f;
-		hojd1=(float)boll.y-300.f;
-		if(bredd1<0)
-			bredd1=0;
-		else if(bredd1>=(bredd-16)*50)
-			bredd1=(bredd-16)*50-1;
+	hojd1=(float)boll.y-300.f;
+	if(bredd1<0)
+	{
+		bredd1=0;
+	}
+	else if(bredd1>=(bredd-16)*50)
+	{
+		bredd1=(bredd-16)*50-1;
+	}
 
-		if(hojd1<0)
-			hojd1=0;
-		else if(hojd1>=(hojd-12)*50)
-			hojd1=(hojd-12)*50-1;
+	if(hojd1<0)
+	{
+		hojd1=0;
+	}
+	else if(hojd1>=(hojd-12)*50)
+	{
+		hojd1=(hojd-12)*50-1;
+	}
 }
 
 void Highscore(SDL_Surface* start,IsPlaying isp[],Players &play)
@@ -307,10 +312,10 @@ void Highscore(SDL_Surface* start,IsPlaying isp[],Players &play)
 		}
 		
 		SDL_PixelFormat *fmt;
-		fmt=surf.ScreenSurface->format;
-		SDL_FillRect(surf.ScreenSurface,0,SDL_MapRGB(fmt,0,0,0));
+		fmt=DSurface.ScreenSurface->format;
+		SDL_FillRect(DSurface.ScreenSurface,0,SDL_MapRGB(fmt,0,0,0));
 		DrawIMGAlpha(start,337,491,133,64,437,491,255,true);
-		DrawIMGAlpha(surf.HighScoreSurface,200,40,420,41,0,0,0,false);
+		DrawIMGAlpha(DSurface.HighScoreSurface,200,40,420,41,0,0,0,false);
 		
 		char chScore[20];
 		for(int i=0; i < 10; ++i)
@@ -322,7 +327,7 @@ void Highscore(SDL_Surface* start,IsPlaying isp[],Players &play)
 		
 		
 						//start=true;
-		SDL_Flip(surf.ScreenSurface);
+		SDL_Flip(Dsurface.ScreenSurface);
 	}
 	ofstream offil("highscore.txt");
 	for(int i=0; i<10; i++)
@@ -330,7 +335,7 @@ void Highscore(SDL_Surface* start,IsPlaying isp[],Players &play)
 		offil << namn[i] << " " << score[i] << endl;
 	}
 	offil.close();
-	SDL_FreeSurface(surf.HighScoreSurface);
+	SDL_FreeSurface(DSurface.HighScoreSurface);
 }
 
 void EscMeny(SDL_Surface *init, Bools &bo, Players &players, IsPlaying isplaying[])
